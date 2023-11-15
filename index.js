338e72f7f12450e0793d2c9229eaebabfb5b9bf8
Elleyse Na <nae122@newschool.edu>
2023-11-15 17:11:32 -0500

    edited file

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-import express from "express"
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3001
+
+app.get('/', (req, res) => {
+    res.send("draw a bird")
+})
+
+app.listen(port, () => {
+    console.log('Example app listening on port ${port}')
+})
 
 console.log("hell")
